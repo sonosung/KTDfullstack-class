d6f839da436640e4976053040df0635093f31190
21-manic-002g <21-manic-002g@21-manic-002>
2024-03-21 13:00:22 +0900

    jungol q506

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,25 @@
+package q506;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int var1 = 170;
+//		double weight = 68.6;
+		
+		System.out.println("My height");
+		System.out.println(var1);
+		System.out.println("My weight");
+		System.out.printf("%f\n", 68.6);
+//		system.out.printf("%.6f", weight)
+
+	}
+}
+//public class Main{
+//public static void main(String[] args) {
+//    int H = 170;
+//    double W = 68.600000;
+//    System.out.printf("My height \n %d \n",H);
+//    System.out.printf("My weight \n %f \n",W);
+//       
+// }
+//}
